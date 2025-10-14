33673c904e70ebb59a497743d96fe5256e63dc23
CheolheeKim <pocahy@icloud.com>
2025-10-14 14:04:13 +0900

    Update run_new.py

--- run_new.py
+++ run_new.py
@@ -8,6 +8,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 import naver_paper_clien as clien
